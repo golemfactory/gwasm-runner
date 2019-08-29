4aa94ac9b357a0afc74cebb67e75238bf4cda7eb
Piotr Chromiec <tworec@golem.network>
2019-08-29 16:48:00 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::path::PathBuf;
+use std::str::FromStr;
+use structopt::*;
+
+#[derive(Debug, Clone)]
+enum Backend {
+    Local,
+    GolemUnlimited,
+    BrassGolem,
+}
+
+impl FromStr for Backend {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Backend, ()> {
+        match s {
+            "Local" => Ok(Backend::Local),
+            "GolemUnlimited" => Ok(Backend::GolemUnlimited),
+            "GU" => Ok(Backend::GolemUnlimited),
+            "Brass" => Ok(Backend::BrassGolem),
+            "Golem" => Ok(Backend::BrassGolem),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug, StructOpt, Clone)]
+struct Opt {
+    /// Backend type to use
+    #[structopt(long, short)]
+    backend: Backend,
+    /// Wasm binary file to run
+    #[structopt(long, short, parse(from_os_str))]
+    wasm_app: PathBuf,
+    /// Wasm app arguments
+    wasm_args: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
