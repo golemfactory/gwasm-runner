a54177632d151b38ef69fa2a5250e4b655a9a078
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2019-09-11 11:03:31 +0200

    Merge step compiles

--- gwasm-api/src/dispatcher.rs
+++ gwasm-api/src/dispatcher.rs
@@ -4,12 +4,11 @@ use std::env;
 
 use failure::{Error, Fail};
 use std::iter::FromIterator;
-use serde::de::Unexpected::Map;
 
 use crate::executor::{Executor, exec_for};
 use crate::splitter::{Splitter, split_into};
-use crate::merger::Merger;
-use crate::taskdef::{FromTaskDef, IntoTaskArg, IntoTaskDef, TaskDef, TaskArg};
+use crate::merger::{Merger, merge_for};
+use crate::taskdef::{FromTaskDef, IntoTaskArg, IntoTaskDef, TaskDef};
 
 
 
@@ -37,7 +36,8 @@ pub enum ApiError {
     },
 }
 
-
+/// =================================== ///
+/// Parameters saving/loading
 
 pub fn save_json(output_file: &Path, json: &serde_json::Value) -> Result<(), Error> {
 
@@ -75,6 +75,14 @@ fn load_task_def(taskdef_file: &Path) -> Result<TaskDef, Error> {
     Ok(serde_json::from_value::<TaskDef>(json)?)
 }
 
+fn load_task_def_vec(taskdef_file: &Path) -> Result<Vec<TaskDef>, Error> {
+    let content = fs::read_to_string(taskdef_file)?;
+    Ok(serde_json::from_str::<Vec<TaskDef>>(&content)?)
+}
+
+/// =================================== ///
+/// Map/Reduce steps
+
 pub fn split_step<S: Splitter<WorkItem = In>, In: IntoTaskDef>(splitter: S, args: &Vec<String>) -> Result<(), Error> {
 
     // TODO: check param len
@@ -99,30 +107,32 @@ pub fn execute_step<E: Executor<In, Out>, In: FromTaskDef, Out: IntoTaskDef >(ex
     save_task_def(&output_desc_path, &output_desc)
 }
 
+pub fn merge_step<M: Merger<In, Out>, In: FromTaskDef, Out: FromTaskDef>(merger: M, args: &Vec<String>) -> Result<(), Error>  {
 
-//pub fn merge_step<MapReduceType: MapReduce>(args: &Vec<String>) -> Result<(), Error>  {
-//
-//    let tasks_params_path = PathBuf::from(args[0].clone());
-//    let tasks_outputs_path = PathBuf::from(args[1].clone());
-//
-//    if args[2] != "--" {
-//        return Err(ApiError::NoSeparator)?;
-//    }
-//
-//    let input_params = load_params_vec::<MapReduceType::ExecuteInput>(&tasks_params_path)?;
-//    let outputs = load_params_vec::<MapReduceType::ExecuteOutput>(&tasks_outputs_path)?;
-//
-//    let in_out_pack = input_params.into_iter()
-//        .zip(outputs.into_iter())
-//        .collect();
-//
-//    let original_args = Vec::from_iter(args[3..].iter().cloned());
-//
-//    MapReduceType::merge(&original_args, &in_out_pack);
-//    Ok(())
-//}
+    let tasks_params_path = PathBuf::from(args[0].clone());
+    let tasks_outputs_path = PathBuf::from(args[1].clone());
 
+    let split_work_dir = tasks_params_path.parent().ok_or(ApiError::NoParent)?;
+    let exec_work_dir = tasks_outputs_path.parent().ok_or(ApiError::NoParent)?;
 
+    if args[2] != "--" {
+        return Err(ApiError::NoSeparator)?;
+    }
+
+    let input_params = load_task_def_vec(&tasks_params_path)?;
+    let outputs = load_task_def_vec(&tasks_outputs_path)?;
+
+    let in_out_pack = input_params.into_iter()
+        .zip(outputs.into_iter())
+        .collect();
+
+    let original_args = Vec::from_iter(args[3..].iter().cloned());
+
+    merge_for(merger, &original_args, in_out_pack, &split_work_dir, &exec_work_dir)
+}
+
+/// =================================== ///
+/// Commands dispatcher - main run function.
 
 pub fn run<S: Splitter<WorkItem = In>,
            E: Executor<S::WorkItem, Out>,
@@ -143,9 +153,9 @@ pub fn run<S: Splitter<WorkItem = In>,
     else if command == "execute" {
         execute_step(executor, &args)
     }
-//    else if command == "merge" {
-//        merge_step::<MapReduceType>(&args)
-//    }
+    else if command == "merge" {
+        merge_step(merger, &args)
+    }
     else {
         Err(ApiError::NoCommand{ command })?
     }
--- gwasm-api/src/merger.rs
+++ gwasm-api/src/merger.rs
@@ -1,3 +1,6 @@
+use std::path::{Path, PathBuf};
+
+use failure::{Error};
 
 use crate::taskdef::{FromTaskArg, FromTaskDef, IntoTaskDef, TaskDef};
 use crate::splitter::{SplitContext};
@@ -8,8 +11,26 @@ pub trait Merger<In: FromTaskDef, Out: FromTaskDef> {
     fn merge(self, args_vec: &Vec<String>, tasks: Vec<(In, Out)>);
 }
 
+pub(crate) fn merge_for<M: Merger<In, Out>, In: FromTaskDef, Out: FromTaskDef>(
+    merger: M,
+    args_vec: &Vec<String>,
+    in_outs_pack: Vec<(TaskDef, TaskDef)>,
+    split_dir: &Path,
+    exec_dir: &Path,
+) -> Result<(), Error> {
 
-impl <In: FromTaskDef, Out: FromTaskDef, F: FnOnce(&Vec<String>, Vec<(In, Out)>)> Merger<In, Out> for F {
+    let in_outs: Result<Vec<(In, Out)>, Error> = in_outs_pack
+        .into_iter()
+        .map(|(params, output)| -> Result<(In, Out), _> {
+            Ok((In::from_task_def(params, split_dir)?, Out::from_task_def(output, exec_dir)?))
+    }).collect();
+
+    merger.merge(args_vec, in_outs?);
+    Ok(())
+}
+
+
+impl<In: FromTaskDef, Out: FromTaskDef, F: FnOnce(&Vec<String>, Vec<(In, Out)>)> Merger<In, Out> for F {
 
     fn merge(self, args_vec: &Vec<String>, tasks: Vec<(In, Out)>) {
         self(args_vec, tasks);
