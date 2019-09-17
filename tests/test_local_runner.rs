51e0bfeee3621cbff72d5c3101539ce3513fc126
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2019-09-17 10:29:05 +0200

    Test mandelbrot example

--- tests/test_local_runner.rs
+++ tests/test_local_runner.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 mod utils;
 
 
@@ -6,25 +8,17 @@ mod utils;
 #[test]
 fn run_madelbrot() {
 
-    let madelbrot_args = vec![ "0.2", "0.35", "0.6", "0.45", "100", "100", "2" ]
+    let mandelbrot_args = vec![ "0.2", "0.35", "0.6", "0.45", "100", "100", "2" ]
         .into_iter()
         .map(String::from)
         .collect();
 
-    let result = utils::run(&utils::get_wasm_mandelbrot_executable(), madelbrot_args);
-    result.unwrap();
+    let execute_in = PathBuf::from("tests/outputs/mandelbrot/");
+    let result = utils::run(&utils::get_wasm_mandelbrot_executable(), &execute_in, mandelbrot_args);
+
+    assert!(result.is_ok(), "Running mandelbrot failed.");
+
+    let out_file = execute_in.join("out.png");
+    assert!(out_file.exists())
 }
 
-
-#[test]
-fn run_madelbrot_lacking_params() {
-
-    // No subtasks number
-    let madelbrot_args = vec![ "0.2", "0.35", "0.6", "0.45", "100", "100" ]
-        .into_iter()
-        .map(String::from)
-        .collect();
-
-    let result = utils::run(&utils::get_wasm_mandelbrot_executable(), madelbrot_args);
-    result.unwrap();
-}
